c750cee23acd593588a9c0180879852b0753fa33
Josias <josiasonesokataborda22@gmail.com>
2025-07-03 14:22:27 -0300

    resolved problems line 13

--- app/main.py
+++ app/main.py
@@ -10,13 +10,18 @@ class Person:
 def create_person_list(people: list) -> list:
     Person.people.clear()
 
-    person_objs = [Person(person_data["name"], person_data["age"]) for person_data in people]
+    person_objs = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
 
     for person_data in people:
         person_obj = Person.people[person_data["name"]]
         if person_data.get("wife"):
-            person_obj.wife = Person.people[person_data["wife"]]
+            wife_name = person_data["wife"]
+            person_obj.wife = Person.people[wife_name]
         if person_data.get("husband"):
-            person_obj.husband = Person.people[person_data["husband"]]
+            husband_name = person_data["husband"]
+            person_obj.husband = Person.people[husband_name]
 
     return person_objs
